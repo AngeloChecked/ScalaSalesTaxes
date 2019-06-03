47469316f13a50e4d07259bb6322663ea4b603f1
XPeppers <xpeppers@XPepperss-MacBook-Pro.local>
2019-06-03 15:16:59 +0200

    migliorato

---
+++ src/main/scala/model/sperimental/GoodChest.scala
@@ -0,0 +1,24 @@
+package model.sperimental
+
+import model.sperimental
+
+class GoodChest(val goods: List[Good]) {
+
+  def totalPrice(): Double = GoodChest.totalPrice(goods)
+  def totalSalesTaxes(): Double = GoodChest.totalSalesTaxes(goods)
+
+  override def toString: String = super.toString
+
+}
+
+object GoodChest {
+  def totalPrice(goods: List[sperimental.Good]): Double =
+    goods.foldLeft(0.0)((a, b) => { b sumPrice a })
+
+  /* restituisce stesso valore dell gli elementi della lista */
+//  def totalPrice2(goods: List[classic.Good]): Double =
+//    goods.reduceLeft((a, b) => { a sumPrice b.price })
+
+  def totalSalesTaxes(goods: List[sperimental.Good]): Double =
+    goods.foldLeft(0.0)((a, b) => { b sumTax a })
+}
